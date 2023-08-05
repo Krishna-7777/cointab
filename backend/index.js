221d815b91c606553fda35ed96daf8bbcd915acf
Krishna-7777 <kchhabraji13@gmail.com>
2023-08-05 12:36:22 +0530

    configured mysql using sequelize

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
 const express = require("express")
-const cors = require("cors");
+const { sequelize } = require("./config/db");
 
 const app=express();
-
-app.use(cors())
 
 app.get('/',(ask,give)=>{
     give.send("Cointab Assignment Backend")
 })
 
-app.listen(4000)
+app.listen(4000, async()=>{
+    try {
+        await sequelize.authenticate();
+        console.log("Connected to the DB and server is running at 4000...");
+    } catch (error) {
+        console.log(error);
+        console.log("Error in connecting to the DB.");
+    }
+})
